0004b718d4373aadb8a5bac50a87a85212b650ed
SpareABtye <104662326+SpareABtye@users.noreply.github.com>
2024-11-07 20:27:57 -0600

    Update urlCrawler.py
    Cleaned up some code.
    hrefs are now properly formatted as urls.

--- urlCrawler.py
+++ urlCrawler.py
@@ -1,39 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
 
-url = 'https://www.codewars.com'
+user_url = input('Enter url: ')
 
+def verify_url(_input):
+    http_head = 'https://'
+    www_head = 'www.'
+    
+    if www_head and http_head not in _input:
+        if www_head not in _input:
+            _input = www_head + _input
+        if http_head not in _input:
+            _input = http_head + _input
+            
+    if www_head not in _input:
+        parts = _input.split('://')
+        parts[0] += '://'
+        parts.insert(1, 'www.')
+        _input = ''.join(parts)
+        
+    return _input
+
+url = verify_url(user_url)
 response = requests.get(url, timeout = 10)
 response.raise_for_status()  # Raises an error for non-200 status codes
 soup = BeautifulSoup(response.text, 'html.parser')
+links = soup.find_all('a', href = True)
+print(f'Status: {response.status_code}')
 
 unvisited_links = []
 visited_links = []
-snd_ctch = []
 
-links = soup.find_all('a', href = True)
-
-print(f'Status: {response.status_code}')
 for link in links:
     href = link.get('href')
-    if (href and ('https://' in href or 'http://' in href) and href not in unvisited_links):
-        unvisited_links.append(link.get('href'))
-    
-print(len(unvisited_links))
+    if len(href) > 1 and 'mailto:' not in href:
+        if 'http' not in href:
+            if href[0] != '/':
+                unvisited_links.append(f'{url}/{href}')
+            else:
+                unvisited_links.append(f'{url}{href}')
+        elif 'http' in href:
+            unvisited_links.append(href)
+                                       
 print('\n'.join(unvisited_links))
-
-while len(unvisited_links) > 0:
-    link_2 = unvisited_links[0]
-    response_2 = requests.get(link_2, timeout = 10)
-    soup_2 = BeautifulSoup(response_2.text, 'html.parser')
-    links_2 = soup_2.find_all('a', href = True)
-
-    for link_1 in links_2:
-        href = link_1.get('href')
-        if (href and ('https://' in href or 'http://' in href) and href not in unvisited_links or visited_links):
-            snd_ctch.append(link_1.get('href'))
-    
-    visited_links.append(link_2)
-    unvisited_links.remove(link_2)
-
